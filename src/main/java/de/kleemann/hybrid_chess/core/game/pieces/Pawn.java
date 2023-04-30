44f41336c456a4baeb94385cbea2f34bd873384b
marcel-gerber <marcelgerber9@gmail.com>
2023-04-30 21:39:10 +0200

    Added boolean, getLegalMoves and move function

--- src/main/java/de/kleemann/hybrid_chess/core/game/pieces/Pawn.java
+++ src/main/java/de/kleemann/hybrid_chess/core/game/pieces/Pawn.java
@@ -1,10 +1,31 @@
 package de.kleemann.hybrid_chess.core.game.pieces;
 
+import de.kleemann.hybrid_chess.core.game.ChessBoard;
 import de.kleemann.hybrid_chess.core.game.Color;
+import de.kleemann.hybrid_chess.core.game.Position;
+
+import java.util.LinkedList;
+import java.util.List;
 
 public class Pawn extends Piece {
 
+    private boolean wasMoved = false;
+
     public Pawn(Color color) {
         super(color);
+    }
+
+    @Override
+    public boolean move(Position newPosition) {
+        wasMoved = true;
+        // TODO
+        return false;
+    }
+
+    public List<Position> getLegalMoves(ChessBoard chessBoard) {
+        LinkedList<Position> legalMoves = new LinkedList<>();
+        Position[][] board = chessBoard.getBoard();
+        // TODO
+        return legalMoves;
     }
 }
