2b0592a61bdf015aa1e81a230878cedfaa344db2
marcel-gerber <marcelgerber9@gmail.com>
2023-05-05 18:29:53 +0200

    Made x and y final; Added setPiece, and isOccupied functions

--- src/main/java/de/kleemann/hybrid_chess/core/game/Position.java
+++ src/main/java/de/kleemann/hybrid_chess/core/game/Position.java
@@ -5,8 +5,8 @@ import de.kleemann.hybrid_chess.core.game.pieces.Piece;
 public class Position {
 
     private Piece piece;
-    private int x;
-    private int y;
+    private final int x;
+    private final int y;
 
     public Position(int x, int y, Piece piece) {
         this.x = x;
@@ -25,4 +25,17 @@ public class Position {
     public Piece getPiece() {
         return piece;
     }
+
+    public void setPiece(Piece piece) {
+        this.piece = piece;
+    }
+
+    public boolean isOccupied() {
+        return this.getPiece() != null;
+    }
+
+    public boolean isOccupiedAndOpponent(Piece piece) {
+        return this.getPiece() != null && this.getPiece().getColor() != piece.getColor();
+    }
+
 }
