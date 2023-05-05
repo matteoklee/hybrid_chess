6c03703dcecb58027d8f976c88bff7179f6c4e1c
marcel-gerber <marcelgerber9@gmail.com>
2023-05-05 18:31:00 +0200

    Added position; Move function done

--- src/main/java/de/kleemann/hybrid_chess/core/game/pieces/Piece.java
+++ src/main/java/de/kleemann/hybrid_chess/core/game/pieces/Piece.java
@@ -1,11 +1,15 @@
 package de.kleemann.hybrid_chess.core.game.pieces;
 
+import de.kleemann.hybrid_chess.core.game.ChessBoard;
 import de.kleemann.hybrid_chess.core.game.Color;
 import de.kleemann.hybrid_chess.core.game.Position;
+
+import java.util.List;
 
 public abstract class Piece {
 
     private final Color color;
+    private Position position;
 
     public Piece(Color color) {
         this.color = color;
@@ -15,8 +19,29 @@ public abstract class Piece {
         return color;
     }
 
-    public boolean move(Position newPosition) {
+    public boolean move(ChessBoard chessBoard, Position newPosition) {
+        List<Position> legalMoves = getLegalMoves(chessBoard);
+        Position[][] board = chessBoard.getBoard();
+
+        for(Position legal : legalMoves) {
+            if(legal.getX() == newPosition.getX() && legal.getY() == newPosition.getY()) {
+                this.setPosition(newPosition);
+                board[newPosition.getY()][newPosition.getX()].setPiece(this);
+                return true;
+            }
+        }
         return false;
     }
 
+    public Position getPosition() {
+        return position;
+    }
+
+    private void setPosition(Position position) {
+        this.position = position;
+    }
+
+    public List<Position> getLegalMoves(ChessBoard chessBoard) {
+        return null;
+    }
 }
