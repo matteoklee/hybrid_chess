1c5a116635193f22419c1cd1c801a1ee3450baa9
marcel-gerber <marcelgerber9@gmail.com>
2023-05-06 16:17:11 +0200

    Knight done

--- src/main/java/de/kleemann/hybrid_chess/core/game/pieces/Knight.java
+++ src/main/java/de/kleemann/hybrid_chess/core/game/pieces/Knight.java
@@ -4,6 +4,7 @@ import de.kleemann.hybrid_chess.core.game.ChessBoard;
 import de.kleemann.hybrid_chess.core.game.Color;
 import de.kleemann.hybrid_chess.core.game.Position;
 
+import java.util.LinkedList;
 import java.util.List;
 
 public class Knight extends Piece {
@@ -14,6 +15,33 @@ public class Knight extends Piece {
 
     @Override
     public List<Position> getLegalMoves(ChessBoard chessBoard) {
-        return null;
+        LinkedList<Position> legalMoves = new LinkedList<>();
+        Position[][] board = chessBoard.getBoard();
+
+        int[] a = {1, 2, 2, 1, -1, -2, -2, -1};
+        int[] o = {2, 1, -1, -2, -2, -1, 1, 2};
+
+        int y = this.getPosition().getY();
+        int x = this.getPosition().getX();
+
+        int num = 0;
+        while(num < 8) {
+            int u = x+o[num], v = y+a[num];
+
+            if(u < 0 || u >= chessBoard.getColumns() || v < 0 || v >= chessBoard.getRows()) {
+                num++;
+                continue;
+            }
+
+            if(board[v][u].isOccupied() && !board[v][u].isOpponent(this)) {
+                num++;
+                continue;
+            }
+
+            legalMoves.add(board[v][u]);
+            num++;
+        }
+
+        return legalMoves;
     }
 }
