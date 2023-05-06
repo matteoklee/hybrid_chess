92a7b57c66b3b20696ffb9e76940a9c17bc8323d
marcel-gerber <marcelgerber9@gmail.com>
2023-05-06 16:16:57 +0200

    King done

--- src/main/java/de/kleemann/hybrid_chess/core/game/pieces/King.java
+++ src/main/java/de/kleemann/hybrid_chess/core/game/pieces/King.java
@@ -4,6 +4,7 @@ import de.kleemann.hybrid_chess.core.game.ChessBoard;
 import de.kleemann.hybrid_chess.core.game.Color;
 import de.kleemann.hybrid_chess.core.game.Position;
 
+import java.util.LinkedList;
 import java.util.List;
 
 public class King extends Piece {
@@ -14,6 +15,22 @@ public class King extends Piece {
 
     @Override
     public List<Position> getLegalMoves(ChessBoard chessBoard) {
-        return null;
+        LinkedList<Position> legalMoves = new LinkedList<>();
+        Position[][] board = chessBoard.getBoard();
+
+        int y = this.getPosition().getY();
+        int x = this.getPosition().getX();
+
+        for(int i = -1; i <= 1; i++) {
+            for(int j = -1; j <= 1; j++) {
+                if(y+i < 0 || y+i >= chessBoard.getRows() || x+j < 0 || x+j >= chessBoard.getColumns()) continue;
+                if(i == 0 && j == 0) continue;
+                if(board[y+i][x+j].isOccupied() && !board[y+i][x+j].isOpponent(this)) continue;
+
+                legalMoves.add(board[y+i][x+j]);
+            }
+        }
+
+        return legalMoves;
     }
 }
