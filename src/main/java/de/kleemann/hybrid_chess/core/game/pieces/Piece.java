95698ddd7054143d92c1f287e7d3f735f8b1ea78
marcel-gerber <marcelgerber9@gmail.com>
2023-05-06 16:14:15 +0200

    Added getDiagonalMoves function Added setPiece of position to constructor Changed equal check of legalMoves

--- src/main/java/de/kleemann/hybrid_chess/core/game/pieces/Piece.java
+++ src/main/java/de/kleemann/hybrid_chess/core/game/pieces/Piece.java
@@ -4,6 +4,7 @@ import de.kleemann.hybrid_chess.core.game.ChessBoard;
 import de.kleemann.hybrid_chess.core.game.Color;
 import de.kleemann.hybrid_chess.core.game.Position;
 
+import java.util.LinkedList;
 import java.util.List;
 
 public abstract class Piece {
@@ -14,6 +15,7 @@ public abstract class Piece {
     public Piece(Color color, Position position) {
         this.color = color;
         this.position = position;
+        this.position.setPiece(this);
     }
 
     public Color getColor() {
@@ -26,7 +28,7 @@ public abstract class Piece {
         Position newPosition = board[y][x];
 
         for(Position legal : legalMoves) {
-            if(legal.getX() == newPosition.getX() && legal.getY() == newPosition.getY()) {
+            if(legal == newPosition) {
                 board[this.getPosition().getY()][this.getPosition().getX()].removePiece();
 
                 this.setPosition(newPosition);
@@ -35,6 +37,64 @@ public abstract class Piece {
             }
         }
         return false;
+    }
+
+    public List<Position> getDiagonalMoves(ChessBoard chessBoard) {
+        LinkedList<Position> legalMoves = new LinkedList<>();
+        Position[][] board = chessBoard.getBoard();
+
+        int y = this.getPosition().getY();
+        int x = this.getPosition().getX();
+
+        // Nord-West-Richtung
+        for(int i = -1; y+i >= 0 && x+i >= 0; i--) {
+            if(board[y+i][x+i].isOccupied()) {
+                if(board[y+i][x+i].isOpponent(this)) {
+                    legalMoves.add(board[y+i][x+i]);
+                    break;
+                }
+                break;
+            }
+            legalMoves.add(board[y+i][x+i]);
+        }
+
+        // Nord-Ost-Richtung
+        for(int i = -1, j = 1; y+i >= 0 && x+j < chessBoard.getColumns(); i--, j++) {
+            if(board[y+i][x+j].isOccupied()) {
+                if(board[y+i][x+j].isOpponent(this)) {
+                    legalMoves.add(board[y+i][x+j]);
+                    break;
+                }
+                break;
+            }
+            legalMoves.add(board[y+i][x+j]);
+        }
+
+        // Süd-West-Richtung
+        for(int i = 1, j = -1; y+i < chessBoard.getRows() && x+j >= 0; i++, j--) {
+            if(board[y+i][x+j].isOccupied()) {
+                if(board[y+i][x+j].isOpponent(this)) {
+                    legalMoves.add(board[y+i][x+j]);
+                    break;
+                }
+                break;
+            }
+            legalMoves.add(board[y+i][x+j]);
+        }
+
+        // Süd-Ost-Richtung
+        for(int i = 1; y+i < chessBoard.getRows() && x+i < chessBoard.getColumns(); i++) {
+            if(board[y+i][x+i].isOccupied()) {
+                if(board[y+i][x+i].isOpponent(this)) {
+                    legalMoves.add(board[y+i][x+i]);
+                    break;
+                }
+                break;
+            }
+            legalMoves.add(board[y+i][x+i]);
+        }
+
+        return legalMoves;
     }
 
     public Position getPosition() {
